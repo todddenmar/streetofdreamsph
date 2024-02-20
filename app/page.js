8cd8ee95570608cdffdfaf55f527c0ef4675ef05
todddenmar <todddenmar@gmail.com>
2024-02-20 15:58:34 +0800

    added about us and mission homepage

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import HeroBanner from './ui/HeroBanner';
 export const metadata = {
   title: 'Street Of Dreams',
@@ -7,6 +8,39 @@ export default function Home() {
   return (
     <main className="grid">
       <HeroBanner />
+      <div className="p-5 max-w-[1500px] mx-auto mt-[50px] md:mt-[100px]">
+        <div className="grid md:grid-cols-2 gap-[50px] relative">
+          <div className="relative">
+            <Image
+              src={'/mission.jpg'}
+              alt="mission"
+              width={400}
+              height={400}
+              className="w-full rounded-tr-full rounded-bl-full z-20 relative"
+            />
+            <div className="absolute inset-0 bg-amber-500 blur-md md:blur-lg z-10 rounded-tr-full rounded-bl-full animate-pulse"></div>
+          </div>
+          <div className="absolute md:relative flex flex-col justify-center inset-0 text-white z-20">
+            <div className="grid gap-[50px]">
+              <div className="md:-ml-[200px] p-5 md:p-10 rounded-lg relative overflow-hidden border-none ">
+                <div className="absolute inset-0 bg-stone-950 bg-opacity-85 "></div>
+                <div className="relative">
+                  <h3 className="text-5xl">Mission</h3>
+                  <p className="mt-5 md:text-xl md:leading-8">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining
+                    essentially unchanged.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
